d1a460ec3129483c34841b387db76cc2e45f239c
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2024-11-02 21:08:41 -0700

    Created Routes to different pages: landing, meeting, summary

---
+++ frontend/src/pages/meeting.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VideoCall from '../components/VideoCall';
+import Notes from '../components/Notes';
+import '../App.css';
+
+export const Meeting = () => {
+  const dailyUrl = 'https://conveneai.daily.co/conveneAI';
+
+  return (
+    <div className="app-container">
+      <div className="video-section">
+        <VideoCall url={dailyUrl} />
+      </div>
+      <div className="notes-section">
+        <Notes />
+      </div>
+    </div>
+  );
+};
+
+export default Meeting;
---
+++ frontend/src/pages/summary.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Summary = () => {
+  return (
+    <div>Summary</div>
+  );
+}
+
+export default Summary;
